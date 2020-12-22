3aa1223413d8a08ecd4897e9c51517bd37a0f8dc
Jasmine <jasylop@gmail.com>
2020-12-22 11:58:55 -0800

    added timeline component to main and commented out console.log and test data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
 
-function App() {
-  console.log(timelineData);
 
+function App() {
+  // console.log(timelineData);
+  // const testData = [{ 
+  //   person: 'Adele Goldberg',
+  //   status: 'In Smalltalk, everything happens somewhere else.',
+  //   timeStamp: '2018-05-18T22:12:03Z'
+  // }]
   // Customize the code below
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="App-title">Application title</h1>
+        <h1 className="App-title">{timelineData.person + '\'s Social Media Feed'}</h1>
       </header>
       <main className="App-main">
+        <Timeline events={timelineData.events}/>
       </main>
     </div>
   );
